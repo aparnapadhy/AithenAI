21769063175de64f7a564a37fe9cec2c164933a1
Aparna Padhy <aparnapadhyap672@gmail.com>
2025-11-02 10:50:23 +0530

    responsiveness improved

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -48,7 +48,7 @@ if(loading){
 
   return (
     <div className='max-w-7xl h-screen overflow-y-scroll mx-auto px-4 sm:px-6 lg:px-8 py-12 '>
-      <h2 className='text-3xl font-semibold mx-82 my-10 xl:mt-30 text-gray-800 dark:text-white'>Credit Plans</h2>
+      <h2 className='text-3xl font-semibold sm:mx-82 my-10 xl:mt-30 text-gray-800 dark:text-white'>Credit Plans</h2>
 
       <div className='flex flex-wrap justify-center gap-8'>
         {plans.map((plan)=>(
