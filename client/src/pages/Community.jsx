a6880e3c7f77c3add3eb40856fd7dc8bd9ddd519
Aparna Padhy <aparnapadhyap672@gmail.com>
2025-11-02 11:03:37 +0530

    UI improved

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -34,7 +34,7 @@ const Community = () => {
 
   return (
     <div className='p-6 pt-12 xl:px-12 2xl:px-20 w-full mx-auto h-full overflow-y-scroll '>
-       <h2 className='text-3xl font-semibold mb-6 text-center sm:text-start text-gray-800 dark:text-white mt-8 sm:mt-0'>Community Images</h2>
+       <h2 className=' text-xl sm:text-3xl font-semibold mb-6 text-center sm:text-start text-gray-800 dark:text-white mt-8 sm:mt-0'>Community Images</h2>
           {images.length > 0 ? (
             <div className='flex flex-wrap max-sm:justify-center gap-5'>
               {images.map((item,index)=>(
